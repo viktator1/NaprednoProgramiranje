ff2f5a3e2c7de571dbb3c3a3ecbb1f2ee973d445
bojantomic <tomicb@fon.rs>
2023-02-28 10:36:33 +0100

    Uvedena klasa Autor, izmenjena klasa Knjiga

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -1,12 +1,14 @@
 package biblioteka;
 
+import java.util.List;
+
 public class Knjiga {
 	
 	private String naslov;
 	
 	private long isbn;
 	
-	private String autori;
+	private List<Autor> autori;
 	
 	private String izdavac;
 
@@ -28,11 +30,11 @@ public class Knjiga {
 		this.isbn = isbn;
 	}
 
-	public String getAutori() {
+	public List<Autor> getAutori() {
 		return autori;
 	}
 
-	public void setAutori(String autori) {
+	public void setAutori(List<Autor> autori) {
 		this.autori = autori;
 	}
 
