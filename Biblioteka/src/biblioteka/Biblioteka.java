78a0398f6bbe6f2b80bd37a474f05decd74aeb3e
bojantomic <tomicb@fon.rs>
2023-03-07 09:09:41 +0100

    Napravljena klasa Biblioteka koja impl interfejs BibliotekaInterfejs

---
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -0,0 +1,39 @@
+package biblioteka;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import biblioteka.interfejs.BibliotekaInterfejs;
+
+public class Biblioteka implements BibliotekaInterfejs {
+
+	private List<Knjiga> knjige = new ArrayList<Knjiga>();
+	
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		knjige.add(knjiga);
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		knjige.remove(knjiga);
+	}
+
+	@Override
+	public List<Knjiga> vratiSveKnjige() {
+		return knjige;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		
+		List<Knjiga> rezultati = new ArrayList<Knjiga>();
+		
+		for (Knjiga k: knjige)
+			if (k.getNaslov().toLowerCase().contains(naslov.toLowerCase().trim()))
+				rezultati.add(k);
+		
+		return rezultati;
+	}
+
+}
